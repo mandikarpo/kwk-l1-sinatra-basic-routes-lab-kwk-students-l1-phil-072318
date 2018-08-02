30b2d265378ff342ca1c773808dec7902718324c
Mandi Karpo <mandi.karpo@gmail.com>
2018-08-02 12:57:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+  
+  #homepage- whatever my local host is 
+  get '/' do 
+    "Hello World"
+  end 
+
+  #localhost/name
+  get '/name' do 
+    "My name is Mandi"
+  end
+
+end 
